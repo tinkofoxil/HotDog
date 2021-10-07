83e74122f5faa0caaff7706c4a6e6f21c76d55be
tinkofoxil <90129819+tinkofoxil@users.noreply.github.com>
2021-10-07 19:27:22 +0300

    Create Football.java

---
+++ Lab 2.1/Football.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Football extends JFrame
+{
+    int milan = 0;
+    int rm = 0;
+    JLabel res = new JLabel("Result: 0 X 0");
+    JLabel last = new JLabel("Last Scorer: N/A");
+    JLabel win = new JLabel("Winner: DRAW");
+    JButton button1 = new JButton("AC Milan");
+    JButton button2 = new JButton("Real Madrid");
+    Football() {
+        super("Football");
+        setLayout(new FlowLayout());
+        setSize(350,400);
+        add(res);
+        add(last);
+        add(win);
+        add(button1);
+        add(button2);
+button1.addActionListener(new ActionListener()  //AC Milan
+    {
+        public void actionPerformed(ActionEvent ae)
+        {
+            milan+=1;
+            res.setText("Result: " + milan + " X " + rm);
+            last.setText("Last Scorer: AC Milan");
+            if (milan>rm) {
+                win.setText("Winner: WIN");
+            }
+            if (milan<rm){
+                win.setText("Winner: LOSE");
+            }
+            if (milan==rm){
+                win.setText("Winner: DRAW");
+            }
+        }
+    });
+button2.addActionListener(new ActionListener() {  //Real Madrid
+    public void actionPerformed(ActionEvent ae)
+    {
+        rm+=1;
+        res.setText("Result: " + milan + " X " + rm);
+        last.setText("Last Scorer: Real Madrid");
+        if (milan>rm) {
+            win.setText("Winner: LOSE");
+        }
+        if (milan<rm){
+            win.setText("Winner: WIN");
+        }
+        if (milan==rm){
+            win.setText("Winner: DRAW");
+        }
+    }
+});
+        setVisible(true);
+    }
+    public static void main(String[]args)
+    {
+        new Football();
+    }
+}
