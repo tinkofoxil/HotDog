ae4ea7785d328f35adaf5388ee43a6ce8e1440b5
tinkofoxil <90129819+tinkofoxil@users.noreply.github.com>
2021-11-13 11:47:18 +0300

    Create App.java

---
+++ Lab3.1/App.java
@@ -0,0 +1,11 @@
+import gui.EmployeeAddForm;
+import gui.EmployeeFrame;
+import model.Employee;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        EmployeeFrame frame = new EmployeeFrame();
+    }
+}
