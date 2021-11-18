432394b3b4546003e7c62e7dad47ae10e056cae2
tinkofoxil <90129819+tinkofoxil@users.noreply.github.com>
2021-11-18 11:47:01 +0300

    Add files via upload

---
+++ Pr6/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.creatPlayer1();
+        game.creatPlayer2();
+
+        game.playGame();
+    }
+}
